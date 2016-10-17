b3bf19e2a757470ea9627ff2a5200b462205dbf1
marcoaros <guoshudong@gmail.com>
2016-10-17 16:19:32 +0800

    * code review

--- app/src/main/java/com/marcoaros/hover/HoverActivity.java
+++ app/src/main/java/com/marcoaros/hover/HoverActivity.java
@@ -1,14 +1,10 @@
-package com.marcoaros.hover.myapplication;
+package com.marcoaros.hover;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.WindowManager;
+
+import com.marcoaros.hover.myapplication.R;
 
 public class HoverActivity extends AppCompatActivity {
 
@@ -20,25 +16,5 @@ public class HoverActivity extends AppCompatActivity {
         toolbar.setTitle("");
         toolbar.setNavigationIcon(null);
         setSupportActionBar(toolbar);
-        //full(false);
-    }
-
-    /**
-     *  控制导航栏的显示与隐藏
-     * @param enable
-     */
-    private void full(boolean enable) {
-        if (enable) {
-            WindowManager.LayoutParams lp = getWindow().getAttributes();
-            lp.flags |= WindowManager.LayoutParams.FLAG_FULLSCREEN;
-            getWindow().setAttributes(lp);
-            getWindow().addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-            //以上代码等效于getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN|WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-        } else {
-            WindowManager.LayoutParams attr = getWindow().getAttributes();
-            // attr.flags &= (~WindowManager.LayoutParams.FLAG_FULLSCREEN);//这个可以解除全屏
-            getWindow().setAttributes(attr);
-            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);//显示导航栏
-        }
     }
 }
